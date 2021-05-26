8a305583cb7eb88a7c9061576d24a2246fd5557c
nishad <nishadkurup@gmail.com>
2021-05-26 13:34:30 +0530

    worked on javascript

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,58 +1,58 @@
-let questionBank= [
+let questions= [
     {
-       question : "Which of the following element is responsible for making the text bold in HTML?",
+       title : "Which of the following element is responsible for making the text bold in HTML?",
        choices : ["<pre>","<a>","<b>","<br>"],
        answer : "<b>"
     },
 
     {
-       question : "CSS is the language we use to style which document.",
+       title : "CSS is the language we use to style which document.",
         choices : ["PHP"," HTML","SQL"," .Net"],
         answer : " HTML"
      },
 
     {
-        question : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        title : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
         choices : [" The User's machine running a Web browser"," The Web server","A central machine deep within Netscape's corporate offices"," . None of the above"],
         answer : " The User's machine running a Web browser"
     },
 
     {
-        question : " Which of the following tag is used to insert a line-break in HTML ? ",
+        title : " Which of the following tag is used to insert a line-break in HTML ? ",
         choices : ["<br>","<a>","<b>","<pre>"],
         answer : "<br>"
     }, 
 
     {
-        question : " External stylesheets are stored in CSS files.",
+        title : " External stylesheets are stored in CSS files.",
         choices : ["TRUE","FALSE","Can be true or false","Can not say"],
         answer : "TRUE"
     },
    
     
     {
-        question : "  Why so JavaScript and Java have similar name? ",
+        title : "  Why so JavaScript and Java have similar name? ",
         choices : ["JavaScript is a stripped-down version of Java","JavaScript's syntax is loosely based on Java's","They both originated on the island of Java","None of the above"],
         answer : "JavaScript's syntax is loosely based on Java's"
     },
         
 
     {
-        question : " Which of the following tag is used for inserting the largest heading in HTML? ",
+        title : " Which of the following tag is used for inserting the largest heading in HTML? ",
         choices : ["<h3>","<h1>","<h5>","<h6>"],
         answer : "<h1>"
     }, 
 
      
     {
-        question : "CSS is the acronym for? ",
+        title : "CSS is the acronym for? ",
         choices : ["Color Style Sheets"," Cascade Sheets Style","Cascade Style Sheet","Cascading Style Sheets"],
         answer : "Cascading Style Sheets"
     }, 
 
 
     {
-        question : " Which JavaScript is also called client-side JavaScript. ",
+        title : " Which JavaScript is also called client-side JavaScript. ",
         choices : ["Microsoft","Navigator","LiveWire","Native"],
         answer : "Navigator"
     }, 
@@ -60,7 +60,7 @@ let questionBank= [
 
 
     {
-        question : " Which JavaScript is also called server-side JavaScript. ",
+        title : " Which JavaScript is also called server-side JavaScript. ",
         choices : ["Microsoft","Navigator","LiveWire","Native"],
         answer : "LiveWire"
     },     
